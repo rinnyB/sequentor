9ac0525357d76213150116bf0f587c4efb1b543f
Mikhail <martmiel@gmail.com>
2018-02-13 17:14:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 setup(
-name = "Sequentor",
+name = "sequentor",
 version = "0.0.2",
 url = "example.com",
 author = "Mikhail Martynenko",
 author_email = "martmiel@gmail.com",
-packages = ["Sequentor"]
+packages = ["sequentor"]
 )
