83ae14e527db21fe6c647d8693ffd55b94141e29
Mikhail <martmiel@gmail.com>
2018-02-16 22:29:48 +0200

    flist tests

--- sequentor/tests/test_FList.py
+++ sequentor/tests/test_FList.py
@@ -26,7 +26,26 @@ def testMap(data, function, expectedData):
 [
     ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], lambda x: x > 5, [6, 7, 8, 9, 10]),
     ([[1, 2, 3], [1, 2], [1,2,3,4], [2]], lambda x: len(x) > 2, [[1,2,3],[1,2,3,4]])
-]
-)
+])
 def testFilter(data, function, expectedData):
     FList(data).filter(function) == expectedData
+
+
+@pytest.mark.parametrize('data, function, expectedData',
+[
+    ('word', lambda x: x.upper(), ["W", "O", "R", "D"]),
+    (['words','about','words'], lambda x: x, ["w","o","r","d","s","a","b","o","u","t","w","o","r","d","s"]),
+    ([1, 2, 3], lambda x: range(0, x), [0, 0, 1, 0, 1, 2])
+])
+def testFlatMap(data, function, expectedData):
+    assert FList(data).flatMap(function) == expectedData
+
+
+@pytest.mark.parametrize('data, function, expectedData',
+[
+    ([1, 2, 3, 4, 5, 6], lambda x, y: x + y,  1 + 2 + 3 + 4 + 5 + 6),
+    ([6, 5, 4, 3, 2, -1], lambda x, y: x - y, 6 - 5 - 4 - 3- 2 - -1 ),
+    ([6 , 4, 2], lambda x, y: x / y, ((6 / 4 ) / 2))
+])
+def testReduce(data, function, expectedData):
+    assert FList(data).reduce(function) == expectedData
