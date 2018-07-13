4094697a1fa606946d2bb79a591fe14f39387da5
Mikhail <martmiel@gmail.com>
2018-07-14 02:11:02 +0300

    FList init & tests

--- sequentor/flist.py
+++ sequentor/flist.py
@@ -1,3 +1,27 @@
-class FList:
-    def __init__(self, *data):
-        raise NotImplementedError
+class FList(list):
+    """
+    Initialize a FList
+    possible inputs to construct FList from:
+    - a number of arguments
+    - a list
+    - a tuple        
+    """
+    def __init__(self, *arr):
+        # arr is always a tuple
+        # for example:
+        # tuple([list],)
+        # tuple((tuple),)
+        # tuple(a,r,g,s)
+        # tuple(map,) or tuple(filter,)
+
+
+        if isinstance(arr, tuple):
+            if len(arr) == 1: 
+                if isinstance(arr[0], (map, filter, list, tuple)):
+                    super().__init__(arr[0])
+            else:
+                super().__init__(arr)
+
+                
+        self.isSorted = False
+
--- sequentor/tests/test_flist.py
+++ sequentor/tests/test_flist.py
@@ -1,11 +1,18 @@
 import pytest
 from sequentor.flist import FList
 
-## Test that FList is initialized successfully
-## for different input types: a single list
-## and a various arguments list
-@pytest.mark.parametrize('data', [[1, 2, 3, 4, 5]])
-def test_FList_init(data):
+# Test that FList is initialized successfully
+# for different input types: a single list
+# and a various arguments list
+
+def test_FList_init_list():
+    data = [1, 2, 3, 4, 5]
     assert FList(data) == data
+
+def test_FList_init_args():
+    data = [1, 2, 3, 4, 5]
     assert FList(*data) == data
 
+def test_FList_init_list_tuples():
+    data = [(1,2), (3,4), (5,6)]
+    assert FList(data) == data
