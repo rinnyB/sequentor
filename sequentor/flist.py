19d3a1e71f4a3dfc7cebff6c6b5fbebb3d3ddbc3
Mikhail <martmiel@gmail.com>
2018-07-18 16:15:53 +0300

    add Map

--- sequentor/flist.py
+++ sequentor/flist.py
@@ -1,10 +1,13 @@
+from sequentor.flist_errors import MapError
+
+
 class FList(list):
     """
     Initialize a FList
     possible inputs to construct FList from:
     - a number of arguments
     - a list
-    - a tuple        
+    - a tuple
     """
     def __init__(self, *arr):
         # arr is always a tuple
@@ -14,14 +17,17 @@ class FList(list):
         # tuple(a,r,g,s)
         # tuple(map,) or tuple(filter,)
 
-
         if isinstance(arr, tuple):
-            if len(arr) == 1: 
+            if len(arr) == 1:
                 if isinstance(arr[0], (map, filter, list, tuple)):
                     super().__init__(arr[0])
             else:
                 super().__init__(arr)
-
-                
         self.isSorted = False
 
+    def map(self, func):
+        try:
+            return FList(map(func, self))
+        except Exception as e:
+            print(e)
+            raise MapError() from e
--- sequentor/tests/test_flist.py
+++ sequentor/tests/test_flist.py
@@ -1,9 +1,14 @@
+# ignore E731
 import pytest
 from sequentor.flist import FList
+from sequentor.flist_errors import MapError
 
-# Test that FList is initialized successfully
-# for different input types: a single list
-# and a various arguments list
+"""
+Test that FList is initialized successfully
+for different input types: a single list
+and a various arguments list
+"""
+
 
 class Test_FlistInit:
     def test_FList_init_list(self):
@@ -15,21 +20,36 @@ class Test_FlistInit:
         assert FList(*data) == data
 
     def test_FList_init_list_tuples(self):
-        data = [(1,2), (3,4), (5,6)]
+        data = [(1, 2), (3, 4), (5, 6)]
         assert FList(data) == data
 
     def test_FList_init_tuple(self):
-        data = (1,2,3,4,5)
+        data = (1, 2, 3, 4, 5)
         assert(FList(data) == list(data))
 
     def test_FList_init_from_map(self):
-        data = [1,2,3,4,5]
-        awaited = [2,3,4,5,6]
+        data = [1, 2, 3, 4, 5]
+        awaited = [2, 3, 4, 5, 6]
         func = lambda x: x + 1
         assert(FList(map(func, data)) == awaited)
 
     def test_FList_init_from_filter(self):
-        data = [1,2,3,4,5]
-        awaited = [1,2,]
+        data = [1, 2, 3, 4, 5]
+        awaited = [1, 2]
         func = lambda x: x < 3
         assert(FList(filter(func, data)) == awaited)
+
+
+class Test_FListMap:
+    def test_FList_map(self):
+        data = [1, 2, 3, 4, 5]
+        awaited = [0.5, 1, 1.5, 2, 2.5]
+        func = lambda x: x / 2
+        assert (FList(data).map(func) == awaited)
+
+    def test_FList_map_raise_exception(self):
+        data = [1, 2, 3, 4, 5]
+        # use inappropriate function
+        func = lambda x: x.lower()
+        with pytest.raises(MapError):
+            FList(data).map(func)
