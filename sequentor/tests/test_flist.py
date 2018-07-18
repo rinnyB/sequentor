5fcffe757b953a8e479d8b771f4fa3983f788809
Mikhail <martmiel@gmail.com>
2018-07-18 15:55:46 +0300

    wrap tests for init into class

--- sequentor/tests/test_flist.py
+++ sequentor/tests/test_flist.py
@@ -5,14 +5,31 @@ from sequentor.flist import FList
 # for different input types: a single list
 # and a various arguments list
 
-def test_FList_init_list():
-    data = [1, 2, 3, 4, 5]
-    assert FList(data) == data
+class Test_FlistInit:
+    def test_FList_init_list(self):
+        data = [1, 2, 3, 4, 5]
+        assert FList(data) == data
 
-def test_FList_init_args():
-    data = [1, 2, 3, 4, 5]
-    assert FList(*data) == data
+    def test_FList_init_args(self):
+        data = [1, 2, 3, 4, 5]
+        assert FList(*data) == data
 
-def test_FList_init_list_tuples():
-    data = [(1,2), (3,4), (5,6)]
-    assert FList(data) == data
+    def test_FList_init_list_tuples(self):
+        data = [(1,2), (3,4), (5,6)]
+        assert FList(data) == data
+
+    def test_FList_init_tuple(self):
+        data = (1,2,3,4,5)
+        assert(FList(data) == list(data))
+
+    def test_FList_init_from_map(self):
+        data = [1,2,3,4,5]
+        awaited = [2,3,4,5,6]
+        func = lambda x: x + 1
+        assert(FList(map(func, data)) == awaited)
+
+    def test_FList_init_from_filter(self):
+        data = [1,2,3,4,5]
+        awaited = [1,2,]
+        func = lambda x: x < 3
+        assert(FList(filter(func, data)) == awaited)
